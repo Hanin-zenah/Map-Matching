5465d2e97526502676939cd37c6c7c54b48c55a8
Hanin-zenah <67774150+Hanin-zenah@users.noreply.github.com>
2020-08-16 20:20:48 +1000

    disc_frechet

--- disc_frechet_v2.cpp
+++ disc_frechet_v2.cpp
@@ -1,127 +1,126 @@
 #include "disc_frechet_v2.h"
+#include "graph.h"
 
-// bool compare_eps(FSedge* edge1, FSedge* edge2) {
-    // return edge1 -> botlneck_val > edge2 -> botlneck_val;
-// }
-// 
-
-unsigned long long int pairing(int n, int m){
-    return 0.5*(n+m)*(n+m+1)+m;
-}
-
-double nodes_dist(node g_nd, node t_nd) {
+double nodes_dist(struct node g_nd, struct node t_nd) {
     double dist = sqrt(pow((t_nd.lat - g_nd.lat), 2) + pow((t_nd.longitude - g_nd.longitude), 2));
     return dist;
 }
 
-double build_node(FSgraph* fsgraph, Graph* graph, Graph* traj, fsnode fsnd, int neighbor_id, int up, int right){
+double build_node(FSgraph* fsgraph, Graph* graph, Graph* traj, fsnode fsnd, int neighbor_id, int up, int right) {
     FSnode fnd;
     FSedge fedge;
 
     //Point* traj
     //traj -> nodes[fsnd.tid] 
-    if (up == 0){
+    if(up == 0) {
          fnd.vid = fsnd.vid;
     }
-    else{
-        fnd.vid =  neighbor_id;
+    else {
+        fnd.vid = neighbor_id;
     }
 
     //fnd.vid = fsnd.vid + up; // up = 1 means get_incident() [i]
     fnd.tid = fsnd.tid + right; // right = 1 means Point next.
     /* test if the corner/node pair already exists, if not, build a new node, but need to build a new edge regardless */
-    if(fsgraph -> pair_dict.find(pairing(fnd.vid, fnd.tid)) == fsgraph -> pair_dict.end()){
-        fnd.fspair = pairing(fnd.vid, fnd.tid);
+    FSpair pair = {fnd.vid, fnd.tid};
+    if(fsgraph -> pair_dict.find(pair) == fsgraph -> pair_dict.end()) {  //if pair not in map 
+        // fnd.fspair = pairing(fnd.vid, fnd.tid);
         fnd.visited = false;
         fnd.dist = nodes_dist(graph -> nodes[fnd.vid], traj -> nodes[fnd.tid]);
         //fnd.edgelist = {}; // initialization? can be overwritten later??
         fsgraph -> fsnodes.push_back(fnd);
-        fsgraph -> pair_dict[fnd.fspair] = &fnd;
+        fsgraph -> pair_dict[pair] = &fnd; //fsgraph -> pair_dict.insert({pair, &fnd});
         fedge.trg = &fnd;
     }
     else {
-         fedge.trg = *fsgraph -> pair_dict.find(pairing(fnd.vid, fnd.tid));
-         fnd.dist = fedge.trg -> dist; 
+        //pair already exists on graph
+        auto it = fsgraph -> pair_dict.find(pair);
+        fedge.trg = it -> second;
+        fnd.dist = fedge.trg -> dist; 
     }
     fedge.edgeid = fsgraph -> fsedges.size() + 2 - up - right;
     fedge.src = &fsnd;
     fedge.botlneck_val = max(fnd.dist, fsgraph -> eps); // the fnd.dist would be the same regardless the prior existence of this new corner
     fsgraph -> fsedges.push_back(fedge);
     return fedge.botlneck_val;
+}
+
+FSpair traversal(FSgraph* fsgraph, Graph* graph, Graph* traj, FSpair corner, 
+                         priority_queue<FSedge*, vector<FSedge*>, Comp_eps>& bigger_eps, stack <FSedge*>& Stack) {
+    
+    auto it = fsgraph -> pair_dict.find(corner);
+    FSnode* fnd = it -> second;
+    vector<int> incidents = get_incident(graph, fnd -> vid);
+    vector<double> btl_neck_vals;
+    double eps = build_node(fsgraph, graph, traj, *fnd, fnd->vid, 0, 1);
+    btl_neck_vals.push_back(eps);
+
+    for(int i; i < incidents.size(); i++) {
+        int neighbour_id  = incidents[i];
+        double eps1 = build_node(fsgraph, graph, traj, *fnd, neighbour_id, 1, 1);
+        btl_neck_vals.push_back(eps1);
+        double eps2 = build_node(fsgraph, graph, traj, *fnd, neighbour_id, 1, 0);
+        btl_neck_vals.push_back(eps2);
     }
 
-unsigned long long int traversal(FSgraph* fsgraph, Graph* graph, Graph* traj, unsigned long long int corner, 
-                         priority_queue<FSedge*, vector<FSedge*>, Comp_eps>& bigger_eps, stack <FSedge*>& Stack){
-    
-   FSnode fnd = *fsgraph -> pair_dict.find(corner);
-   vector<int> incidents = get_incident(graph, fnd.vid);
-   vector<double> btl_neck_vals;
-   double eps = build_node(fsgraph, graph, traj, fnd, fnd.vid, 0, 1);
-   btl_neck_vals.push_back(eps);
-
-   for (int i; i < incidents.size(); i++){
-       int neighbour_id  = incidents[i];
-       
-       double eps1 = build_node(fsgraph, graph, traj, fnd, neighbour_id, 1, 1);
-       btl_neck_vals.push_back(eps1);
-       double eps2 = build_node(fsgraph, graph, traj, fnd, neighbour_id, 1, 0);
-       btl_neck_vals.push_back(eps2);
-   }
-
-   int size = fsgraph -> fsnodes.size();
-   queue <FSedge*> temp_queue;
-   for(int i = 0; i < btl_neck_vals.size(); i++) {
-       if(btl_neck_vals[i] > fsgraph -> eps) {
-            /*store these local eps for potential global min eps increase
-            store the edges with higher eps to be accessed later if needed */
-            bigger_eps.push(&fsgraph -> fsedges[fsgraph -> fsedges.size() - i - 1]);
+    int size = fsgraph -> fsnodes.size();
+    queue <FSedge*> temp_queue;
+    for(int i = 0; i < btl_neck_vals.size(); i++) {
+        if(btl_neck_vals[i] > fsgraph -> eps) {
+                /*store these local eps for potential global min eps increase
+                store the edges with higher eps to be accessed later if needed */
+                bigger_eps.push(&fsgraph -> fsedges[fsgraph -> fsedges.size() - i - 1]);
         }
-       else { /* only store the current 3 outgoing edges, if they meet the condition;
+        else { /* only store the current 3 outgoing edges, if they meet the condition;
                  refresh at each iteration */
            temp_queue.push(&fsgraph -> fsedges[fsgraph -> fsedges.size() - i - 1]);
         }
     }
-    //go forward with the traversal 
-    unsigned long long int next_fspair;
-    //if there are no more available edges in the current cell
-    if(temp_queue.empty()) {
-       if(Stack.empty()) {
-           //case 1: 
-           //if there are no more readily traversable edges in the freespace graph
-           //update the eps (leash length)
-           FSedge* min_eps_fedge = bigger_eps.top();
-           bigger_eps.pop();
-           fsgraph -> eps = min_eps_fedge -> botlneck_val;
-           FSnode* next_nd = min_eps_fedge -> trg; //does the .trg need to be a pointer? does the bigger_eps need to be a queue of pointers?
-           next_nd -> visited = true; //change the actual flag on memory and not the copy of it?
-           next_fspair = pairing(next_nd -> vid, next_nd -> tid);
-       }
-       else { 
-           //case 2: 
-           //cannot proceed with the current cell but doesn't need to change eps yet
-           FSedge* back_edge = Stack.top(); //back to the nearest edge that is still reachable and unvisited with this current eps
-           Stack.pop();
-           FSnode* next_nd = back_edge -> trg;
-           next_nd -> visited = true; 
-           next_fspair = pairing(next_nd -> vid, next_nd -> tid);
+        //go forward with the traversal 
+        FSpair next_fspair;
+        //if there are no more available edges in the current cell
+        if(temp_queue.empty()) {
+            if(Stack.empty()) {
+                //case 1: 
+                //if there are no more readily traversable edges in the freespace graph
+                //update the eps (leash length)
+                FSedge* min_eps_fedge = bigger_eps.top();
+                bigger_eps.pop();
+                fsgraph -> eps = min_eps_fedge -> botlneck_val;
+                FSnode* next_nd = min_eps_fedge -> trg; //does the .trg need to be a pointer? does the bigger_eps need to be a queue of pointers?
+                next_nd -> visited = true; //change the actual flag on memory and not the copy of it?
+                next_fspair.first = next_nd -> vid;
+                next_fspair.second = next_nd -> tid;
 
+            }
+            else { 
+                //case 2: 
+                //cannot proceed with the current cell but doesn't need to change eps yet
+                FSedge* back_edge = Stack.top(); //back to the nearest edge that is still reachable and unvisited with this current eps
+                Stack.pop();
+                FSnode* next_nd = back_edge -> trg;
+                next_nd -> visited = true; 
+                next_fspair.first = next_nd -> vid;
+                next_fspair.second = next_nd -> tid;
+
+            }
         }
-    }
-   else { 
-       // case 3: proceed to a node within the cell, favouring diagonal movement
-       FSedge* selected_edge = temp_queue.front(); //the first one in is always the diagonal move, then right move then up move.
-       temp_queue.pop();
-       while(!temp_queue.empty()) {
-           //FSedge* reachable_edge = temp_queue.top();
-           Stack.push(temp_queue.front());
-           temp_queue.pop();
-       }
+    else { 
+        // case 3: proceed to a node within the cell, favouring diagonal movement
+        FSedge* selected_edge = temp_queue.front(); //the first one in is always the diagonal move, then right move then up move.
+        temp_queue.pop();
+        while(!temp_queue.empty()) {
+            //FSedge* reachable_edge = temp_queue.top();
+            Stack.push(temp_queue.front());
+            temp_queue.pop();
+        }
 
-       FSnode* next_nd = selected_edge -> trg; //.trg is a FS node.
-       next_nd -> visited = true;
-       next_fspair = pairing(next_nd -> vid, next_nd -> tid);
-   }
-   return next_fspair;
+        FSnode* next_nd = selected_edge -> trg; //.trg is a FS node.
+        next_nd -> visited = true;
+        next_fspair.first = next_nd -> vid;
+        next_fspair.second = next_nd -> tid;
+    }
+    return next_fspair;
 }
        
 double min_eps(Graph* graph, Graph* traj, FSgraph* fsgraph){
@@ -133,16 +132,19 @@ double min_eps(Graph* graph, Graph* traj, FSgraph* fsgraph){
     priority_queue<FSedge*, vector<FSedge*>, Comp_eps> bigger_eps;
     stack <FSedge*> Stack; 
     /* building the starting node (V0,T0) */
-    fsgraph -> eps = nodes_dist(graph -> nodes[0] /* this needs to be the closest found node on the graph for the map matching*/, traj -> nodes[0]); //change this to be the closest node 
+                                /* this needs to be the closest found node on the graph for the map matching*/
+    fsgraph -> eps = nodes_dist(graph -> nodes[0], traj -> nodes[0]); //change this to be the closest node 
     fnd.vid = graph -> nodes[0].id;
     fnd.tid = traj -> nodes[0].id;
-    fnd.fspair = pairing(fnd.vid, fnd.tid);
+    // fnd.fspair = pairing(fnd.vid, fnd.tid);
     fnd.visited = true;
     //fnd.edgelist = {0,1,2};// can be overwritten later??
     fsgraph -> fsnodes.push_back(fnd);
-    fsgraph -> pair_dict[fnd.fspair] = &fnd;
-    while (fsgraph -> pair_dict.find(fnd.fspair) -> tid < m) {
-         fnd.fspair = traversal(fsgraph, graph, traj, fnd.fspair, bigger_eps, Stack);
+    FSpair pair = {fnd.vid, fnd.tid};
+    fsgraph -> pair_dict[pair] = &fnd;
+
+    while (fsgraph->pair_dict.find(pair)->second->tid < m) {
+         pair = traversal(fsgraph, graph, traj, pair, bigger_eps, Stack);
     }
     return fsgraph -> eps;
 }
@@ -187,7 +189,7 @@ void read_file(string file_name, Graph* graph) {
     }}
 
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 
     if(argc < 2) {
         cerr << "Not enough arguments; please provide a file name next to the program name to be read\n\nExample: ./a.out saarland.txt\n";
--- disc_frechet_v2.h
+++ disc_frechet_v2.h
@@ -18,8 +18,8 @@
 
 using namespace std;
 
-typedef struct fsnode{
-    unsigned long long int fspair; // are fsnode IDs and edge ID neccesary?
+typedef struct fsnode {
+    // unsigned long long int fspair; //are fsnode IDs and edge ID neccesary?
     int vid;
     int tid;
     double dist;
@@ -28,7 +28,7 @@ typedef struct fsnode{
 } FSnode;
 
 
-typedef struct fsedge{
+typedef struct fsedge {
     int edgeid;
     FSnode* src; //how do I point to a FSnode without using a sort of ID
     FSnode* trg;
@@ -36,23 +36,52 @@ typedef struct fsedge{
 } FSedge;
 
 
+typedef struct FSPair_key {
+    //this pair will store Vid and Tid as a pair to be used as a key for the hashmap 
+    int first; //vid
+    int second; //tid
+
+    bool operator==(const struct FSPair_key& other) const { 
+        return (first == other.first
+            && second == other.second);
+    }
+} FSpair;
+
+struct KeyHash {
+    size_t operator()(const FSpair& k) const {
+        using std::size_t;
+        using std::hash;
+        using std::string;
+
+        return ((hash<int>()(k.first)
+                ^ (hash<int>()(k.second) << 1)) >> 1);
+
+        //or we can use 
+        // 0.5*(n+m)*(n+m+1)+m;
+        //or any hash function we come up with later
+    }
+
+};
+
 typedef struct static_fsgraph {
     double eps; //the min traversal distance, initial = distance(v1, t1) // global leashlength value for the freespace graph
-    unordered_map<unsigned long long int, FSnode*> pair_dict; 
-    vector<struct fsnode> fsnodes;
-    vector<struct fsedge> fsedges;
+    unordered_map<FSpair, FSnode*, KeyHash> pair_dict; 
+    vector<FSnode> fsnodes;
+    vector<FSedge> fsedges;
 } FSgraph;
 
+
+/* to sort the min priority queue */
 struct Comp_eps {
     bool operator()(const FSedge* edge1, const FSedge* edge2) const{
         return edge1->botlneck_val > edge2->botlneck_val;
     }
 };
-/* to sort the min priority queue */
-// bool compare_eps(FSedge* edge1, FSedge* edge2);
 
 /* Cantor pairing function */
-unsigned long long int pairing(int n, int m);
+// unsigned long long int pairing(int n, int m);
+
+
 
 /* distance of Vi, Tj in a corner */
 double nodes_dist(node g_nd, node t_nd);
@@ -64,5 +93,5 @@ double min_eps(Graph* graph, Graph* traj, FSgraph* fsgraph);
 double build_node(FSgraph* fsgraph, fsnode fsnd, int neighbor_id, int up, int right);
 
 /* given a nodes pair on a FS graph, returns the node pair after the next traversal */
-unsigned long long int traversal(FSgraph* fsgraph, unsigned long long int corner, Graph* graph, Graph* traj, priority_queue<FSedge*, vector<FSedge*>, Comp_eps>& bigger_eps, stack <FSedge*>& Stack);
+FSpair traversal(FSgraph* fsgraph, FSpair corner, Graph* graph, Graph* traj, priority_queue<FSedge*, vector<FSedge*>, Comp_eps>& bigger_eps, stack <FSedge*>& Stack);
 #endif

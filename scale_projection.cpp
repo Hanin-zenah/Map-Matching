8aaa388f9f825cad6481a34277984ea91aeba30f
lolahu <60481094+lolahu@users.noreply.github.com>
2020-10-18 14:01:08 +1100

    fix projection

--- scale_projection.cpp
+++ scale_projection.cpp
@@ -18,35 +18,45 @@ double Euc_distance::lon_mercator_proj(double lon, double lon_min) {
     return (lon-lon_min) * M_PI / 180.0;
 }
 
+// need to make an corner case where the area goes through +-180 degree in longitute, maybe long_max - long_min cannot be larger than 180?
+
 double Euc_distance::lat_mercator_proj(double lat, double lat_min) {
-    double lat_r = (lat-lat_min) * M_PI / 180.0;
-    double x = log(tan(0.25 * M_PI + 0.5 * lat_r));
+    double lat_r = (lat-lat_min) * M_PI / 180.0; 
+    double x = log(tan(0.25 * M_PI + 0.5 * lat_r)) - log(tan(0.25 * M_PI));
     return x;
 }
 
-double Euc_distance::lon_mercator_proj_scale(double lon, double lon_min, double y_scale) {
-    return (lon-lon_min) * M_PI * y_scale / 180.0;
-}
-
-double Euc_distance::lat_mercator_proj_scale(double lat, double lat_min, double x_scale) {
-    double lat_r = (lat-lat_min) * M_PI * x_scale/ 180.0;
-    double x = log(tan(0.25 * M_PI + 0.5 * lat_r));
-    return x;
-}
-
-double Euc_distance::euc_dist(double x1, double y1, double x2, double y2, double x_scale, double y_scale) {
-    double dist = sqrt(pow((y2-y1)*x_scale,2.0)+pow((x2-x1)*y_scale,2.0));
+double Euc_distance::euc_dist(double lat1, double lon1, double lat2, double lon2) {
+    double dist = sqrt(pow((lon2-lon1),2.0)+pow((lat2-lat1),2.0));
     return dist;
 }
 
-void Euc_distance::calc_edge_cost(Graph* graph, double x_scale, double y_scale) {
+void Euc_distance::calc_edge_cost(Graph* graph, double lat_scale, double lon_scale) {
     //calculate the cost for every edge in the graph
+    for(int i = 0; i < graph -> n_nodes; i++) {
+    graph -> nodes[i].lat = lat_mercator_proj(graph -> nodes[i].lat, graph -> min_lat) * lat_scale;
+    graph -> nodes[i].longitude = lon_mercator_proj(graph -> nodes[i].longitude, graph -> min_long) * lon_scale;
+    }
+
+    // graph -> min_lat = ed.lat_mercator_proj(graph -> min_lat, graph -> min_lat) * lat_scale;
+    // graph -> max_lat = ed.lat_mercator_proj(graph -> max_lat, graph -> min_lat) * lat_scale; 
+    // graph -> min_long = ed.lon_mercator_proj(graph -> min_long, graph -> min_long) * lon_scale;
+    // graph -> max_long = ed.lon_mercator_proj(graph -> max_long, graph -> min_long) * lon_scale;
+
     for(int i = 0; i < graph -> n_edges; i++) {
         struct node src;
         struct node trgt;
         src = graph -> nodes[graph -> edges[i].srcid];
         trgt = graph -> nodes[graph -> edges[i].trgtid];
-        graph -> edges[i].cost = euc_dist(src.lat, src.longitude, trgt.lat, trgt.longitude, x_scale, y_scale);
+        graph -> edges[i].cost = euc_dist(src.lat, src.longitude, trgt.lat, trgt.longitude);
     }
 }
 
+
+void flip_map(Graph* graph){
+    if(graph -> max_long - graph -> min_long > 180){
+        //fliping the bounding box?
+    }
+
+
+}
--- scale_projection.h
+++ scale_projection.h
@@ -35,13 +35,15 @@ class Euc_distance {
 
         double lat_mercator_proj (double lat, double lat_min);
 
-        double lon_mercator_proj_scale(double lon, double lon_min, double y_scale);
+        // double lon_mercator_proj_scale(double lon, double lon_min, double y_scale);
+// 
+        // double lat_mercator_proj_scale(double lat, double lat_min, double x_scale);
 
-        double lat_mercator_proj_scale(double lat, double lat_min, double x_scale);
+        double euc_dist(double lat1, double lon1, double lat2, double lon2); //, double lat_scale, double lon_scale);
 
-        double euc_dist(double y1, double x1, double y2, double x2, double x_scale, double y_scale);
+        void calc_edge_cost(Graph* graph, double lat_scale, double lon_scale);
 
-        void calc_edge_cost(Graph* graph, double x_scale, double y_scale);
+        void flip_map(Graph* graph);
 
 };
 

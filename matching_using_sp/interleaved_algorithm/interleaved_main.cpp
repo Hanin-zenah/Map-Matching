255641292180b797b43b607e393f97d4c5aa3f1e
Hanin-zenah <67774150+Hanin-zenah@users.noreply.github.com>
2021-08-02 20:25:27 +1000

    modified main to accept user input from argv

--- matching_using_sp/interleaved_algorithm/interleaved_main.cpp
+++ matching_using_sp/interleaved_algorithm/interleaved_main.cpp
@@ -1,9 +1,15 @@
 #include "interleaved.h"
 
 int main(int argc, char** argv) {
+    if(argc < 4) {
+        cerr << "Not enough arguments. Program accepts 3 command line arguments as follows: \
+        \n\nExample: ./preprocess <preprocessed graph file path> <trajectory file path> <trajectory index>\n";
+        return 1;
+    }
+    
     /* read processed graph from a given file */
     Graph after_graph = GRAPH_INIT;
-    read_processed_graph("../graphs/saarland_all_preprocessed.txt", &after_graph);
+    read_processed_graph(argv[1], &after_graph);
     double lat_min = after_graph.original_min_lat;
     double lon_min = after_graph.original_min_long;
     double lat_scale = after_graph.lat_scale;
@@ -14,43 +20,24 @@ int main(int argc, char** argv) {
     double grid_size = 250.00;
     builder.make_grids(&after_graph, &grid, grid_size);
     
-    vector<int> indices; 
-    // indices.push_back(0);
-    indices.push_back(7);
-    // indices.push_back(8);
-    // indices.push_back(9);
-    // indices.push_back(23);
-    // indices.push_back(31);
-    // indices.push_back(19);
-    // indices.push_back(24);
-    // indices.push_back(15);
-    // indices.push_back(458);
-    // indices.push_back(69);
-    // indices.push_back(91);
-    // indices.push_back(4);
-
     Traj t;
     Traj_subsample ts;
-    for(int i = 0; i < indices.size(); i++) {
-        Trajectory traj = t.read_trajectory_k("../trajectories/saarland-geq50m-clean-unmerged-2016-10-09-saarland.binTracks", indices[i], lon_min, lat_min, lat_scale, lon_scale);
-        t.calc_traj_edge_cost(&traj);
-        ts.subsample_traj(&traj, 15);
-        // write_traj(&traj, "traj_frechet_with_sub.dat");
-        cout << "traj size: " << traj.length <<endl;
-        auto t1 = chrono::high_resolution_clock::now(); 
+    Trajectory traj = t.read_trajectory_k(argv[2], stoi(argv[3]), lon_min, lat_min, lat_scale, lon_scale);
+    t.calc_traj_edge_cost(&traj);
+    ts.subsample_traj(&traj, 15);
 
-        pair<double, double> eps_cost = interleaved_matching(&after_graph, &traj, &grid);
+    cout << "traj size: " << traj.length <<endl;
+    auto t1 = chrono::high_resolution_clock::now(); 
 
-        auto t2 = chrono::high_resolution_clock::now();
+    pair<double, double> eps_cost = interleaved_matching(&after_graph, &traj, &grid);
 
-        auto duration = chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+    auto t2 = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
 
-        cout<<"final eps: "<< eps_cost.first <<  " final path cost: "<< eps_cost.second << endl;
-        cout << "Duration in milliseconds: " << duration << endl << endl;
-        grid.curr_range = 0;
+    cout<<"final eps: "<< eps_cost.first <<  " final path cost: "<< eps_cost.second << endl;
+    cout << "Duration in milliseconds: " << duration << endl << endl;
+    grid.curr_range = 0;
 
-        t.cleanup_trajectory(&traj);
-    }
-
+    t.cleanup_trajectory(&traj);
     return 0;
 }
--- matching_using_sp/naive_algorithm/naive_main.cpp
+++ matching_using_sp/naive_algorithm/naive_main.cpp
@@ -1,9 +1,14 @@
 #include "naive.h"
 
 int main(int argc, char** argv) {
+    if(argc < 4) {
+        cerr << "Not enough arguments. Program accepts 3 command line arguments as follows: \
+        \n\nExample: ./preprocess <preprocessed graph file path> <trajectory file path> <trajectory index>\n";
+        return 1;
+    }
     /* read processed graph from a given file */
     Graph after_graph = GRAPH_INIT;
-    read_processed_graph("../../saarland_all_preprocessed.txt", &after_graph);
+    read_processed_graph(argv[1], &after_graph); 
     double lat_min = after_graph.original_min_lat;
     double lon_min = after_graph.original_min_long;
     double lat_scale = after_graph.lat_scale;
@@ -12,43 +17,26 @@ int main(int argc, char** argv) {
     Build_grid builder;
     Grid grid = GRID_INIT;
     builder.make_grids(&after_graph, &grid, GRID_SIZE);
-    
-    vector<int> indices; 
-    indices.push_back(0);
-    // indices.push_back(7);
-    // indices.push_back(8);
-    // indices.push_back(9);
-    // indices.push_back(23);
-    // indices.push_back(31);
-    // indices.push_back(19);
-    // indices.push_back(24);
-    // indices.push_back(10);
-    // indices.push_back(15);
-    // indices.push_back(4);
-    // indices.push_back(91);
 
     Traj t;
     Traj_subsample ts;
-    for(int i = 0; i < indices.size(); i++) {
-        Trajectory traj = t.read_trajectory_k("../../trajectories/saarland-geq50m-clean-unmerged-2016-10-09-saarland.binTracks", indices[i], lon_min, lat_min, lat_scale, lon_scale);
-        t.calc_traj_edge_cost(&traj);
-        ts.subsample_traj(&traj, 15);
+    Trajectory traj = t.read_trajectory_k(argv[2], stoi(argv[3]), lon_min, lat_min, lat_scale, lon_scale);
+    t.calc_traj_edge_cost(&traj);
+    ts.subsample_traj(&traj, 15);
 
-        cout << "traj size: " << traj.length <<endl;
-        auto t1 = chrono::high_resolution_clock::now(); 
+    cout << "traj size: " << traj.length <<endl;
+    auto t1 = chrono::high_resolution_clock::now(); 
 
-        pair<double, double> eps_cost = naive_matching(&after_graph, &traj, &grid);
+    pair<double, double> eps_cost = naive_matching(&after_graph, &traj, &grid);
 
-        auto t2 = chrono::high_resolution_clock::now();
+    auto t2 = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
 
-        auto duration = chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+    cout<<"final eps: "<< eps_cost.first <<  " final path cost: "<< eps_cost.second << endl;
+    cout << "Duration in milliseconds: " << duration << endl << endl;
+    grid.curr_range = 0;
 
-        cout<<"final eps: "<< eps_cost.first <<  " final path cost: "<< eps_cost.second << endl;
-        cout << "Duration in milliseconds: " << duration << endl << endl;
-        grid.curr_range = 0;
-
-        t.cleanup_trajectory(&traj);
-    }
+    t.cleanup_trajectory(&traj);
 
     return 0;
 }
